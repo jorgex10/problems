8f52dcfbcd1c026ba8cebd345426e75438901ae4
Jorge Calvo G. <jorgeecg10@gmail.com>
2023-11-04 22:51:24 -0500

    sums_into_array.rb

--- sums_into_array.rb
+++ sums_into_array.rb
@@ -1,15 +1,16 @@
-=begin
-  Crea una función que encuentre todas las combinaciones de los números
-  de una lista que suman el valor objetivo.
-  - La función recibirá una lista de números enteros positivos
-    y un valor objetivo.
-  - Para obtener las combinaciones sólo se puede usar
-    una vez cada elemento de la lista (pero pueden existir
-    elementos repetidos en ella).
-  - Ejemplo: Lista = [1, 5, 3, 2],  Objetivo = 6
-    Soluciones: [1, 5] y [1, 3, 2] (ambas combinaciones suman 6)
-    (Si no existen combinaciones, retornar una lista vacía)
-=end
+# frozen_string_literal: true
+# rubocop:disable Style/Documentation
+
+#   Crea una función que encuentre todas las combinaciones de los números
+#   de una lista que suman el valor objetivo.
+#   - La función recibirá una lista de números enteros positivos
+#     y un valor objetivo.
+#   - Para obtener las combinaciones sólo se puede usar
+#     una vez cada elemento de la lista (pero pueden existir
+#     elementos repetidos en ella).
+#   - Ejemplo: Lista = [1, 5, 3, 2],  Objetivo = 6
+#     Soluciones: [1, 5] y [1, 3, 2] (ambas combinaciones suman 6)
+#     (Si no existen combinaciones, retornar una lista vacía)
 
 class SumFinder
   def initialize(array, target)
@@ -21,7 +22,7 @@ class SumFinder
   def find_sums
     @array.sort!
     find_sum_into_subarrays(0, @target, [])
-    
+
     @results.inspect
   end
 
@@ -29,15 +30,13 @@ class SumFinder
 
   def find_sum_into_subarrays(start, target, combination)
     # with solution
-    if target == 0 
+    if target.zero?
       @results << combination.dup
       return
     end
 
     # with no solution
-    if target < 0 || start == @array.length
-      return
-    end
+    return if target.negative? || start == @array.length
 
     # backtraking
     for index in (start..@array.length) do
@@ -45,7 +44,7 @@ class SumFinder
 
       new_target = target - @array[index].to_i
       new_combination = combination.push(@array[index])
-      
+
       find_sum_into_subarrays(index + 1, new_target, new_combination)
       combination.pop
     end
